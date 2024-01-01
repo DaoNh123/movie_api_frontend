74cad1f89613930f939ce6d5868e4f3d10b3c166
quan0506 <quanphamm0506@gmail.com>
2024-01-01 22:33:40 +0700

    Home,ComingSoon:100%,Details,Category:50%

--- js/comingsoon.js
+++ js/comingsoon.js
@@ -1,19 +1,3 @@
-/**    NOTIFICATION  **/
-// cua so pop up
-const popup = document.querySelector('.popup');
-const close = document.querySelector('.close-popup')
-
-window.onload = function(){
-  setTimeout(function(){
-    popup.style.display = "block"
-
-     // Add some time delay to show popup
-  },1500)
-}
-close.addEventListener('click',()=>{
-   popup.style.display = "none"
-})
-
 // SCROLL HEADER
 let header = document.querySelector('header');
 let menu = document.querySelector('#menu-icon');
@@ -33,47 +17,8 @@ window.onscroll = () =>{
   navbar.classList.remove('active');
 }
 
-// SLIDE 
-var swiper = new Swiper(".home", {
-    spaceBetween: 30,
-    centeredSlides: true,
-    autoplay: {
-      delay: 6000,
-      disableOnInteraction: false,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-  }); 
 
-  // SLIDE WRAPPER
-  var swiper = new Swiper(".coming-container",{
-    spaceBetween: 20,
-    loop:true,
-    autoplay:{
-      delay:1500,
-      disableOnInteraction:false,
-    },
-    centeredSlides:true,
-    breakpoints:{
-      0:{
-        slidesPerView:2,
-      },
-      568:{
-        slidesPerView:3,
-      },
-      768:{
-        slidesPerView:4,
-      },
-      968:{
-        slidesPerView:5,
-      },
-    },
-  });
-
-
-// Render API và phân trang
+// Render API COMMING SOON ra ngoài trình duyệt và phân trang
 const fetchApi = async (api) => {
   const response = await fetch(api);
   const data = await response.json();
@@ -87,7 +32,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const pageSize = 10; 
   let currentPage = 1; 
 
-  fetchApi("http://localhost:8080/api/movies/now-showing?page=0&size=28").then((data) => {
+  fetchApi("http://localhost:8080/api/movies/coming-soon?pages=0&size=18").then((data) => {
     let movies = data.content;
     const totalMovies = movies.length;
     const totalPages = Math.ceil(totalMovies / pageSize); 
@@ -100,7 +45,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
       let html = `
         <h2 class="heading">
-          Opening This Week
+          COMING SOON
         </h2>
         <div class="grid-container">
       `;
@@ -109,16 +54,16 @@ document.addEventListener("DOMContentLoaded", () => {
         html += `
           <div class="box" id="movieDetails">
             <div class="box-img">
-              <a href="details.html?id=${movie.id}"><img src="${movie.posterUrl}" alt=""></a>
+              <a href="detailscomingsoon.html?id=${movie.id}"><img src="${movie.posterUrl}" alt=""></a>
               <div class="overlay">
                 <div class="button-container">
-                  <a href="choosepay.html" class="book-button">Book</a>
-                  <a href="details.html?id=${movie.id}" class="detail-button">Detail</a>
+                  
+                  <a href="detailscomingsoon.html?id=${movie.id}" class="detail-button">Detail</a>
                 </div>
               </div>
               <div class="movie-label">${movie.movieLabel}</div>
             </div>
-            <a href="details.html?id=${movie.id}"><h3 id="movieName">${movie.movieName}</h3></a>
+            <a href="detailscomingsoon.html?id=${movie.id}"><h3 id="movieName">${movie.movieName}</h3></a>
             <span id="duration">${movie.duration} min </span>
           </div>
         `;
