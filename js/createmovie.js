5fed4539f2bd17b01c6f02ded4f1879ef74407fc
Quân Phạm <quanphamm0506@gmail.com>
2024-01-08 16:20:25 +0000

    Update createmovie.js

--- js/createmovie.js
+++ js/createmovie.js
@@ -1,37 +1,35 @@
-// const form = document.querySelector('form');
 
-// form.addEventListener('submit', (event) => {
-//   console.log('Submitting');
-//   event.preventDefault();
+class CreateMovieRequest {
+  constructor(movieName, language, duration, imdbRatings,movieLabel,iframe,openingTime,closingTime,categoryList,description) {
+    this.movieName = movieName;
+    this.language = language;
+    this.duration =duration;
+    this.imdbRatings = imdbRatings;
+    this.movieLabel = movieLabel;
+    this.iframe = iframe;
+    this.openingTime = openingTime;
+    this.closingTime = closingTime;
+    this.categoryList =categoryList;
+    this.description=description;
+  }
+}
 
-//   const formData = new FormData(form);
-//   const jsonData = JSON.stringify(Object.fromEntries(formData));
+document.addEventListener("DOMContentLoaded", function () {
+  let submitBtn = document.querySelector("btnSubmit"); 
 
-//   debugger;
+  if (submitBtn) { 
+    submitBtn.addEventListener("click", (e) => {
+      e.preventDefault();
+      
+    });
+ 
+  const createMovieRequestJson = JSON.stringify(new CreateMovieRequest(movieName, language, duration, imdbRatings,movieLabel,iframe,openingTime,closingTime,categoryList,description));
 
-//   fetch('http://localhost:8080/api/movies/', {
-//     method: 'POST',
-//     headers: {
-//       'Content-Type': 'application/json',
-//        Accept: "application/json",
-//     },
-//     body: jsonData
-//   })
-//   .then(response => response.json())
-//   .then(data => {
-//       console.log(data)
-//   })
-//   .catch(error => console.log(error));
-
-//   form.reset();
-// });
-
-document.querySelector("form").addEventListener("submit", function(e) {
-  e.preventDefault();
-
+  const posterFileInput = document.getElementById("posterFileInput"); 
+  const posterFile = posterFileInput.files[0];
   const movieName = document.querySelector(".movieName input").value;
   const language = document.querySelector(".language select").value;
-  const posterFile = document.querySelector(".poster input").files[0];
+  // const posterFile = document.querySelector(".poster input").files[0];
   const duration = document.querySelector(".duration input").value;
   const imdbRatings = document.querySelector(".imdbRatings input").value;
   const movieLabel = document.querySelector(".movieLabel select").value;
@@ -42,35 +40,28 @@ document.querySelector("form").addEventListener("submit", function(e) {
   const description = document.querySelector(".description input").value;
 
   const formData = new FormData();
-  formData.append("movieName", movieName);
-  formData.append("language", language);
-  formData.append("poster", posterFile);
-  formData.append("duration", duration);
-  formData.append("imdbRatings", imdbRatings);
-  formData.append("movieLabel", movieLabel);
-  formData.append("iframe", iframe);
-  formData.append("openingTime", openingTime);
-  formData.append("closingTime", closingTime);
-  formData.append("categoryList", categoryList);
-  formData.append("description", description);
-
-  // debugger;
-
+  formData.append('poster', posterFile);
+  //debugger;
+  
+  formData.append('createMovieRequest', new Blob([createMovieRequestJson], { type: 'application/json' }));
+  
   fetch("http://localhost:8080/api/movies", {
     method: "POST",
     body: formData,
   })
-    .then(function(response) {
+    .then((response) => {
       if (!response.ok) {
         throw new Error("HTTP error! Status: " + response.status);
       }
-      return response.json();
+      return response.json(); 
     })
-    .then(function(data) {
+    .then((data) => {
+    
       console.log("Response data:", data);
     })
-    .catch(function(error) {
+    .catch((error) => {
+
       console.error("Error:", error);
     });
-    
+  };
 });
