1d74e6d10ee1d600c4c1c528cc4ed205d68cb9dd
Be-better-everyday <125204550+Be-better-everyday@users.noreply.github.com>
2024-01-14 21:50:14 +0700

    processing createOrder

--- js/choosepay.js
+++ js/choosepay.js
@@ -10,161 +10,162 @@
   }
 
 
-  document.addEventListener('DOMContentLoaded', function () {
-    var seats = document.querySelectorAll('.seat');
+  // document.addEventListener('DOMContentLoaded', function () {
+  //   var seats = document.querySelectorAll('.seat');
   
-    // Gọi API để lấy dữ liệu trạng thái chỗ ngồi từ máy chủ
-    fetch('http://localhost:8080/api/slots/11')
-      .then(response => response.json())
-      .then(data => {
-        // Cập nhật trạng thái chỗ ngồi dựa trên dữ liệu nhận được từ máy chủ
-        data.forEach(slot => {
-          var seatElement = document.querySelector('.row .' + slot.seat);
-          if (seatElement) {
-            seatElement.classList.add(slot.status);
+  //   // Gọi API để lấy dữ liệu trạng thái chỗ ngồi từ máy chủ
+  //   fetch('http://localhost:8080/api/slots/12')
+  //     .then(response => response.json())
+  //     .then(data => {
+  //       // Cập nhật trạng thái chỗ ngồi dựa trên dữ liệu nhận được từ máy chủ
+  //       data.forEach(slot => {
+  //         var seatElement = document.querySelector('.row .' + slot.seat);
+  //         if (seatElement) {
+  //           seatElement.classList.add(slot.status);
   
-            // Vô hiệu hóa chỗ đã có người ngồi
-            if (slot.status === 'occupied') {
-              seatElement.removeEventListener('click', seatClickHandler);
-            }
-          }
-        });
-      })
-      .catch(error => console.error('Lỗi khi lấy dữ liệu chỗ ngồi:', error));
+  //           // Vô hiệu hóa chỗ đã có người ngồi
+  //           if (slot.status === 'occupied') {
+  //             seatElement.removeEventListener('click', seatClickHandler);
+  //           }
+  //         }
+  //       });
+  //     })
+  //     .catch(error => console.error('Lỗi khi lấy dữ liệu chỗ ngồi:', error));
   
-    seats.forEach(function (seat) {
-      seat.addEventListener('click', seatClickHandler);
-    });
+  //   seats.forEach(function (seat) {
+  //     seat.addEventListener('click', seatClickHandler);
+  //   });
   
-    function seatClickHandler() {
-      // Kiểm tra xem chỗ còn trống hay không trước khi chuyển đổi lớp đã chọn
-      if (!this.classList.contains('occupied')) {
-        this.classList.toggle('selected');
-        updateTotal();
-      }
-    }
+  //   function seatClickHandler() {
+  //     // Kiểm tra xem chỗ còn trống hay không trước khi chuyển đổi lớp đã chọn
+  //     if (!this.classList.contains('occupied')) {
+  //       this.classList.toggle('selected');
+  //       updateTotal();
+  //     }
+  //   }
   
-    function updateTotal() {
-      var selectedSeats = document.querySelectorAll('.selected');
-      var vipSeatPrice = 15;
-      var normalSeatPrice = 12;
-      var selectedSeatPrice =
-        selectedSeats.length *
-        (document.getElementById('chair').value === '15' ? vipSeatPrice : normalSeatPrice);
-      document.getElementById('totalAmount').innerText = '$' + selectedSeatPrice;
-    }
-  });
+  //   function updateTotal() {
+  //     var selectedSeats = document.querySelectorAll('.selected');
+  //     var vipSeatPrice = 15;
+  //     var normalSeatPrice = 12;
+  //     var selectedSeatPrice =
+  //       selectedSeats.length *
+  //       (document.getElementById('chair').value === '15' ? vipSeatPrice : normalSeatPrice);
+  //     document.getElementById('totalAmount').innerText = '$' + selectedSeatPrice;
+  //   }
+  // });
   
-  // Modal box
-  const section = document.querySelector('section'),
-    overlay = document.querySelector('.overlay'),
-    showBtn = document.querySelector('.show-modal'),
-    closeBtn = document.querySelector('.close-btn');
+  // // Modal box
+  // const section = document.querySelector('section'),
+  //   overlay = document.querySelector('.overlay'),
+  //   showBtn = document.querySelector('.show-modal'),
+  //   closeBtn = document.querySelector('.close-btn');
   
-  showBtn.addEventListener('click', () => section.classList.add('active'));
-  closeBtn.addEventListener('click', () => section.classList.remove('active'));
+  // showBtn.addEventListener('click', () => section.classList.add('active'));
+  // closeBtn.addEventListener('click', () => section.classList.remove('active'));
   
 
 
-  document.addEventListener('DOMContentLoaded', function () {
-    // Hàm để lấy dữ liệu API
-    let getApiData = async (apiLink) => {
-      const response = await fetch(apiLink);
-      const data = await response.json();
-      return data;
-    };
+  // document.addEventListener('DOMContentLoaded', function () {
+  //   // Hàm để lấy dữ liệu API
+  //   let getApiData = async (apiLink) => {
+  //     const response = await fetch(apiLink);
+  //     const data = await response.json();
+  //     return data;
+  //   };
   
-    // Hàm để tạo SeatClass
-    class SeatClass {
-      constructor(seatClassName, price) {
-        this.seatClassName = seatClassName;
-        this.price = price;
-      }
-    }
+  //   // Hàm để tạo SeatClass
+  //   class SeatClass {
+  //     constructor(seatClassName, price) {
+  //       this.seatClassName = seatClassName;
+  //       this.price = price;
+  //     }
+  //   }
   
-    // Hàm để tạo Seat
-    class Seat {
-      constructor(seatId, seatName, status, seatClass) {
-        this.seatId = seatId;
-        this.seatName = seatName;
-        this.row = seatName[0];
-        this.column = seatName.slice(1);
-        this.status = status;
-        this.seatClass = new SeatClass(seatClass.seatClassName, seatClass.price);
-      }
-      toString() {
-        return `Seat ID: ${this.seatId}
-          Seat Name: ${this.seatName}
-          Status: ${this.status}
-          Row: ${this.row}
-          Column: ${this.column}
-          Seat Class Name: [
-            seatClassName: ${this.seatClass.seatClassName}
-            price: ${this.seatClass.price}
-          ]`;
-      }
-    }
+  //   // Hàm để tạo Seat
+  //   class Seat {
+  //     constructor(seatId, seatName, status, seatClass) {
+  //       this.seatId = seatId;
+  //       this.seatName = seatName;
+  //       this.row = seatName[0];
+  //       this.column = seatName.slice(1);
+  //       this.status = status;
+  //       this.seatClass = new SeatClass(seatClass.seatClassName, seatClass.price);
+  //     }
+  //     toString() {
+  //       return `Seat ID: ${this.seatId}
+  //         Seat Name: ${this.seatName}
+  //         Status: ${this.status}
+  //         Row: ${this.row}
+  //         Column: ${this.column}
+  //         Seat Class Name: [
+  //           seatClassName: ${this.seatClass.seatClassName}
+  //           price: ${this.seatClass.price}
+  //         ]`;
+  //     }
+  //   }
   
-    // Hàm để lấy Map của các dãy ghế
-    let getMapRowSeat = async () => {
-      return getApiData("http://localhost:8080/api/slots/11")
-        .then((data) => {
-          return data.map((seat) => new Seat(seat.seatId, seat.seatName, seat.status, seat.seatClass));
-        })
-        .then((listSeat) => {
-          const seatMap = new Map();
+  //   // Hàm để lấy Map của các dãy ghế
+  //   let getMapRowSeat = async () => {
+  //     return getApiData("http://localhost:8080/api/slots/11")
+  //       .then((data) => {
+  //         return data.map((seat) => new Seat(seat.seatId, seat.seatName, seat.status, seat.seatClass));
+  //       })
+  //       .then((listSeat) => {
+  //         const seatMap = new Map();
   
-          listSeat.forEach((seat) => {
-            const key = seat.row;
-            if (!seatMap.has(key)) {
-              seatMap.set(key, []);
-            }
-            seatMap.get(key).push(seat);
-          });
+  //         listSeat.forEach((seat) => {
+  //           console.log(seat);
+  //           const key = seat.row;
+  //           if (!seatMap.has(key)) {
+  //             seatMap.set(key, []);
+  //           }
+  //           seatMap.get(key).push(seat);
+  //         });
   
-          console.log(seatMap);
-          return seatMap;
-        });
-    };
+  //         console.log(seatMap);
+  //         return seatMap;
+  //       });
+  //   };
 
-    // Fetch dữ liệu ghế và cập nhật giao diện
-    getMapRowSeat()
-    .then(mapRowSeat => {
-      let rows = mapRowSeat.keys();
-      for (const row of rows) {
-        // Tạo phần tử div để chứa ghế trong mỗi dãy
-        let rowContainer = document.createElement('div');
-        rowContainer.classList.add('row-container');
+  //   // Fetch dữ liệu ghế và cập nhật giao diện
+  //   getMapRowSeat()
+  //   .then(mapRowSeat => {
+  //     let rows = mapRowSeat.keys();
+  //     for (const row of rows) {
+  //       // Tạo phần tử div để chứa ghế trong mỗi dãy
+  //       let rowContainer = document.createElement('div');
+  //       rowContainer.classList.add('row-container');
 
-        // Hiển thị số dãy
-        let rowLabel = document.createElement('div');
-        rowLabel.classList.add('row-label');
-        rowLabel.textContent = `Row ${row}`;
-        rowContainer.appendChild(rowLabel);
+  //       // Hiển thị số dãy
+  //       let rowLabel = document.createElement('div');
+  //       rowLabel.classList.add('row-label');
+  //       rowLabel.textContent = `Row ${row}`;
+  //       rowContainer.appendChild(rowLabel);
 
-        // Hiển thị các ghế trong dãy
-        let seatsInRow = mapRowSeat.get(row);
-        seatsInRow.forEach(seat => {
-          let seatElement = document.createElement('div');
-          seatElement.classList.add('seat');
-          seatElement.textContent = seat.seatName;
+  //       // Hiển thị các ghế trong dãy
+  //       let seatsInRow = mapRowSeat.get(row);
+  //       seatsInRow.forEach(seat => {
+  //         let seatElement = document.createElement('div');
+  //         seatElement.classList.add('seat');
+  //         seatElement.textContent = seat.seatName;
 
-          // Thêm màu sắc tương ứng dựa trên trạng thái ghế
-          if (seat.status === 'occupied') {
-            seatElement.classList.add('occupied');
-          } else {
-            seatElement.classList.add('available');
-            seatElement.addEventListener('click', seatClickHandler);
-          }
+  //         // Thêm màu sắc tương ứng dựa trên trạng thái ghế
+  //         if (seat.status === 'occupied') {
+  //           seatElement.classList.add('occupied');
+  //         } else {
+  //           seatElement.classList.add('available');
+  //           seatElement.addEventListener('click', seatClickHandler);
+  //         }
 
-          rowContainer.appendChild(seatElement);
-        });
+  //         rowContainer.appendChild(seatElement);
+  //       });
 
-        // Thêm dãy ghế vào container chính
-        seatsContainer.appendChild(rowContainer);
-      }
-    })
-    .catch(error => console.error('Error fetching seat data:', error));
-  });
+  //       // Thêm dãy ghế vào container chính
+  //       seatsContainer.appendChild(rowContainer);
+  //     }
+  //   })
+  //   .catch(error => console.error('Error fetching seat data:', error));
+  // });
   
   
---
+++ js/choosingSeat.js
@@ -0,0 +1,343 @@
+let getApi = async (apiLink) => {
+  const response = await fetch(apiLink);
+  const data = await response.json();
+  return data;
+};
+// Handling choosing Slot View
+class SlotResponse {
+  constructor(id, startTime, endTime, theaterRoom) {
+    this.id = id;
+    this.startTime = startTime;
+    this.endTime = endTime;
+    this.theaterRoom = theaterRoom;
+
+    // Extracting hour and minute from startTime
+    const [datePart, timePart] = startTime.split("@");
+    this.date = datePart;
+
+    const [hour, minute] = timePart.split(":");
+    this.startTimeWithinDay = `${hour}:${minute}`;
+  }
+
+  toString() {
+    return `SlotResponse(id=${this.id}, startTime=${this.startTime}, endTime=${this.endTime}, theaterRoom=${this.theaterRoom})`;
+  }
+}
+
+class OverallResponse {
+  constructor(resultSize, slotResponses) {
+    this.resultSize = resultSize;
+    this.slotResponses = slotResponses.map(
+      (slot) => new SlotResponse(slot.id, slot.startTime, slot.endTime, slot.theaterRoom)
+    );
+  }
+
+  toString() {
+    return `OverallResponse(resultSize=${this.resultSize}, slotResponses=[${this.slotResponses
+      .map((slot) => slot.toString())
+      .join(", ")}])`;
+  }
+}
+
+let getMovieId = async () => {
+  let urlParams = await new URLSearchParams(window.location.search);
+  let movieId = await decodeURIComponent(urlParams.get("movie-id"));
+  return movieId;
+};
+
+let getSlotsByMovieId = async () => {
+  let movieId = await getMovieId();
+  let slotResponse = await getApi(`http://localhost:8080/api/movies/${movieId}/slots`)
+    .then((data) => new OverallResponse(data.resultSize, data.slotResponses))
+    .then((res) => {
+      console.log(res);
+      return res;
+    });
+
+  return slotResponse.slotResponses;
+};
+
+let slotResponsesArray = async () => {
+  return getSlotsByMovieId().then((slotList) => {
+    return slotList;
+  });
+};
+
+// Convert the array to a Map with day of startTime as the key
+const slotResponsesMap = async () => {
+  return slotResponsesArray().then((slotResponsesArray) => {
+    return slotResponsesArray.reduce((map, slotResponse) => {
+      const unsolvedDay = slotResponse.startTime.split("@")[0];
+
+      const inputDate = new Date(unsolvedDay);
+
+      // Format the Date object as a new string
+      const options = { day: "numeric", month: "numeric", year: "numeric" };
+      const day = inputDate.toLocaleDateString("en-GB", options);
+
+      // Extracting the day part
+      if (!map.has(day)) {
+        map.set(day, []);
+      }
+      map.get(day).push(slotResponse);
+      return map;
+    }, new Map());
+  });
+};
+
+// Log the resulting Map
+slotResponsesMap().then((map) => console.log(map));
+slotResponsesMap().then((slotResponsesMap) => {
+  let selectDayTag = document.querySelector("select#day");
+
+  const defaultOptionTag = document.createElement("option");
+  defaultOptionTag.value = "#";
+  selectDayTag.appendChild(defaultOptionTag);
+
+  for (const date of slotResponsesMap.keys()) {
+    const optionTag = document.createElement("option");
+    optionTag.setAttribute("date", date);
+    optionTag.innerText = date;
+
+    selectDayTag.appendChild(optionTag);
+  }
+
+  selectDayTag.addEventListener("change", (e) => {
+    const chosenDate = selectDayTag.value;
+    const choosingTimeSelectTag = document.querySelector("#choosing-time");
+
+    console.log(choosingTimeSelectTag);
+
+    choosingTimeSelectTag.innerHTML = "";
+    const slotListInChosenDate = slotResponsesMap.get(chosenDate);
+    const defaultOptionTimeTag = document.createElement("option");
+    defaultOptionTag.value = "";
+    choosingTimeSelectTag.appendChild(defaultOptionTag);
+
+    for (const slot of slotListInChosenDate) {
+      const optionTimeTag = document.createElement("option");
+      optionTimeTag.setAttribute("time", slot.startTimeWithinDay);
+      optionTimeTag.setAttribute("slot-id", slot.id);
+      optionTimeTag.value = slot.id;
+
+      optionTimeTag.innerText = slot.startTimeWithinDay + " - " + slot.id + " " + " - " + slot.theaterRoom;
+
+      choosingTimeSelectTag.appendChild(optionTimeTag);
+    }
+
+    choosingTimeSelectTag.addEventListener("change", async (e) => {
+      e.preventDefault();
+      console.log(choosingTimeSelectTag.value);
+
+      if (choosingTimeSelectTag.value != "") {
+        console.log(choosingTimeSelectTag.value);
+        renderChoosingSeatView(choosingTimeSelectTag.value);
+      }
+    });
+  });
+
+  console.log(selectDayTag);
+});
+
+// ************ Handling choosing Seat View
+
+class SeatClass {
+  constructor(seatClassName, price) {
+    this.seatClassName = seatClassName;
+    this.price = price;
+  }
+}
+
+class Seat {
+  constructor(seatId, seatName, status, seatClass) {
+    this.seatId = seatId;
+    this.seatName = seatName;
+    this.row = seatName[0];
+    this.column = seatName.slice(1);
+    this.status = status;
+    this.seatClass = new SeatClass(seatClass.seatClassName, seatClass.price);
+  }
+  toString() {
+    return `Seat ID: ${this.seatId}
+      Seat Name: ${this.seatName}
+      Status: ${this.status}
+      Row: ${this.row}
+      Column: ${this.column}
+      Seat Class Name: [
+        seatClassName: ${this.seatClass.seatClassName}
+        price: ${this.seatClass.price}
+      ]`;
+  }
+}
+
+let getMapRowSeat = async (slotId) => {
+  return getApi(`http://localhost:8080/api/slots/${slotId}`)
+    .then((data) => {
+      return data.map((seat) => new Seat(seat.seatId, seat.seatName, seat.status, seat.seatClass));
+    })
+    .then((listSeat) => {
+      const seatMap = new Map();
+
+      listSeat.forEach((seat) => {
+        const key = seat.row;
+        if (!seatMap.has(key)) {
+          seatMap.set(key, []);
+        }
+        seatMap.get(key).push(seat);
+      });
+
+      return seatMap;
+    });
+};
+
+const renderChoosingSeatView = async (movieId) => {
+  if (movieId == "") {
+    let seatContainerInside = document.querySelector(".seat-container-inside");
+    seatContainerInside.innerHTML = "";
+    return;
+  }
+
+  getMapRowSeat(movieId).then((mapRowSeat) => {
+    let seatContainerInside = document.querySelector(".seat-container-inside");
+    seatContainerInside.innerHTML = "";
+    console.log(seatContainerInside);
+
+    let rows = mapRowSeat.keys();
+    for (const row of rows) {
+      let rowDiv = document.createElement("div");
+      rowDiv.classList.add("row");
+      rowDiv.setAttribute("row", row);
+      // row ==> render a "row"
+
+      let seatsInRow = mapRowSeat.get(row);
+      for (const seat of seatsInRow) {
+        let seatDiv = document.createElement("div");
+        seatDiv.classList.add("seat");
+        if (seat.status == "BOOKED") seatDiv.classList.add("occupied");
+
+        seatDiv.innerText = seat.seatName;
+        seatDiv.setAttribute("seat-name", seat.seatName);
+        seatDiv.setAttribute("seat-class-name", seat.seatClass.seatClassName);
+        seatDiv.setAttribute("price", seat.seatClass.price);
+        seatDiv.setAttribute("seat-id", seat.seatId);
+
+        if (seat.seatClass.seatClassName == "NOR") seatDiv.classList.add("nor");
+        if (seat.seatClass.seatClassName == "VIP") seatDiv.classList.add("vip");
+
+        if (!seatDiv.classList.contains("occupied")) {
+          seatDiv.addEventListener("click", (e) => {
+            let totalAmountElement = document.getElementById("totalAmount");
+            let total = parseInt(totalAmountElement.innerText.replace(/,/g, ""));
+
+            seatDiv.classList.toggle("selected");
+
+            if (!seatDiv.hasAttribute("selected")) {
+              total += parseInt(seatDiv.getAttribute("price"));
+              seatDiv.setAttribute("selected", "");
+            } else {
+              total -= parseInt(seatDiv.getAttribute("price"));
+              seatDiv.removeAttribute("selected");
+            }
+
+            totalAmountElement.innerText = total.toLocaleString("en-US");
+          });
+        }
+
+        rowDiv.appendChild(seatDiv);
+      }
+      seatContainerInside.appendChild(rowDiv);
+    }
+  });
+};
+
+// ******************** Sending createOrder Form
+const openPaymentFormBtn = document.querySelector("#open-payment-form");
+
+const createOrderBtn = document.querySelector("#create-order-btn");
+const closePaymentFormBtn = document.querySelector("#close-payment-form");
+
+const paymentForm = document.querySelector(".payment-form");
+const overlayOfPaymentForm = document.querySelector("#overlay");
+
+const fullNameInput = document.querySelector("#full-name-input");
+const emailInput = document.querySelector("#email-input");
+const addressInput = document.querySelector("#address-input");
+const dobInput = document.querySelector("#dob-input");
+
+console.log(createOrderBtn);
+console.log(closePaymentFormBtn);
+console.log(paymentForm);
+console.log(dobInput);
+
+closePaymentFormBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  paymentForm.classList.add("hidden");
+  overlayOfPaymentForm.classList.add("hidden");
+});
+
+openPaymentFormBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  const choosingTimeSelectTag = document.querySelector("#choosing-time");
+  console.log(choosingTimeSelectTag.value);
+
+  let seatIdList = [];
+  const chosenSeat = document.querySelectorAll(".seat-container-inside .selected");
+  for (const seat of chosenSeat) {
+    seatIdList.push(seat.getAttribute("seat-id"));
+  }
+  console.log(seatIdList);
+
+  if (choosingTimeSelectTag.value == "") alert("Please choose Day and Time to create a Order!");
+  else if (seatIdList.length == 0) alert("Please choose a seat before create a order!");
+  else {
+    paymentForm.classList.remove("hidden");
+    overlayOfPaymentForm.classList.remove("hidden");
+    createOrderBtn.setAttribute("seat-id-list", seatIdList);
+    createOrderBtn.setAttribute("slot-id", choosingTimeSelectTag.selectedOptions[0].getAttribute("slot-id"));
+  }
+});
+
+class OrderRequest {
+  constructor(customerName, customerEmail, customerAddress, customerAge,  slotId, seatIdList) {
+    this.customerName = customerName;
+    this.customerEmail = customerEmail;
+    this.customerAddress = customerAddress;
+    this.customerAge = customerAge;
+    this.slotId = slotId;
+    this.seatIdList = seatIdList;
+  }
+
+  toString() {
+    return `OrderRequest { 
+      customerName: ${this.customerName}, 
+      customerEmail: ${this.customerEmail}, 
+      customerAddress: ${this.customerAddress}, 
+      customerAge: ${this.customerAge},
+      slotId: ${this.slotId}, 
+      seatIdList: [${this.seatIdList.join(", ")}]
+    }`;
+  }
+}
+
+createOrderBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  const seatIdList = createOrderBtn.getAttribute("seat-id-list").split(",");
+  console.log(seatIdList);
+
+  // Calculate age of customer
+  const birthDate = new Date(dobInput.value);
+  const currentDate = new Date();
+  let age = currentDate.getFullYear() - birthDate.getFullYear();
+
+  if (
+    currentDate.getMonth() < birthDate.getMonth() ||
+    (currentDate.getMonth() === birthDate.getMonth() && currentDate.getDate() < birthDate.getDate())
+  ) {
+    age--;
+  }
+  // End calculate "age"
+
+  const orderRequest = new OrderRequest(fullNameInput.value, emailInput.value, addressInput.value, age, createOrderBtn.getAttribute('slot-id'), seatIdList);
+
+  console.log(orderRequest);
+});
